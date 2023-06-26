f265b96935f343b5257c1d791c9ad744e72e25c0
root <root@DESKTOP-22QUIGB.localdomain>
2023-06-26 17:43:41 -0300

    Setting the initial setup

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # pokeback
+- _Pokemon Api docuemntation_: https://pokeapi.co/docs/v2#pokemon-section 
---
+++ app.js
@@ -0,0 +1,22 @@
+// Modules
+  const express = require('express')
+// Configs
+  // Express
+    const app = express()
+          app.use(express.json())
+  //DotEnv
+    require('dotenv').config();
+// Rotas
+  // Main
+    app.get('/', (req, res) => {
+      res.send('Okay')
+    })
+
+// Server
+app.listen(process.env.PORT, ()=>{
+  try {
+    console.log(`Server runnig on: ${process.env.URL}${process.env.PORT}`)
+  } catch (error) {
+    console.log(`has an error on your code: ${error}`)
+  }
+})
